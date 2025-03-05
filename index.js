b0f605900e30ecb303a5e10eb8ae0e89dfea2f49
unknown <ortizmoncada2006@gmail.com>
2025-03-05 11:01:50 -0500

    carpeta public

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 app.use("/factus/customer", routercustomer);
 app.use("/factus/items", routeritems);
 app.use("/factus/facturas", routerfacturas);
