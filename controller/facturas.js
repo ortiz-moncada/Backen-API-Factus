58c23fdd90e9474858246f25523cece13d985f8c
unknown <ortizmoncada2006@gmail.com>
2025-04-02 11:44:41 -0500

    finalizado

--- controller/facturas.js
+++ controller/facturas.js
@@ -1,39 +1,30 @@
 import mongoose from "mongoose";
 import facturaModels from "../models/facturas.js";
+//crear
+const postFactura = async (req, res) => {
+    try {
+        const data = req.body.data;
+        const Factura = new facturaModels({data});
+        await Factura.save();
+        console.log("data front",data)
+        res.json({ message: "factura registrada", factura: Factura });
+    } catch (error) {
+        res.status(400).json({ message: "falla en la operacion" });
+        console.log(error)
+    }
+}
 
-
-
-    //crear
-    const postFactura = async (req, res) => {
-        try {
-            const  {reference_code, obsevation, payment_form, payment_due_date, email,phone,
-                payment_method_code,customer,items,billing_period} = req.body;
-    
-                const Factura = new facturaModels({
-                    reference_code, obsevation, payment_form, payment_due_date, email,phone,
-                payment_method_code,customer,items,billing_period
-                });
-            await Factura.save();
-            res.json(Factura);
-        } catch (error) {
-            res.status(400).json({ message: "falla en la operacion" });
-            console.log(error)
-        }
+//listar factura
+const getFactura = async (req, res) => {
+    try {
+        const factura = await facturaModels.find();
+        res.json(factura)
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ error: "error al mirar facturas" })
     }
 
-    //listar factura
-    const getFactura = async (req, res) => {
-        try {
-            const factura = await facturaModels.find();
-            res.json(factura)
-        }catch (error) {
-            console.error(error);
-            res.status(400).json({error:"error al mirar facturas"})
-        }
-
-    }
-
-
+}
 
 
 export {
--- models/facturas.js
+++ models/facturas.js
@@ -2,18 +2,7 @@ import mongoose from "mongoose";
 
 
 const facturaSchema = mongoose.Schema({
-    reference_code:{type:String, require:true },
-    obsevation:{type:String},
-    payment_form:{type:Number, require:true},
-    payment_due_date:{type:Date,require:true},
-    payment_method_code:{type:Number, require:true},
-    customer:{type:Object, required:true, ref: 'customer'}, //recibe objeto completo con datos del cliente
-    items:{type:Array, required:true, ref: 'items'},
-    billing_period:[{
-        start_date:{type:Date, require:true},
-        end_date:{type:Date, require:true}
-    }],
-   
+   data:{type:mongoose.Schema.Types.Mixed, required:true}
 })
 
-export default mongoose.model("Factuc",facturaSchema);
+export default mongoose.model("Factuc",facturaSchema);  
